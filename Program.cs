1eb50b73b7a1cb027fc4027a80858a997ed32dac
Ravindra <yravindraredy@gmail.com>
2023-09-03 22:27:40 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string:");
+        string inp = Console.ReadLine();    
+        var op = RemoveDuplicates(inp);
+        Console.WriteLine(op);
+    }
+
+    public static string RemoveDuplicates(string str)
+    {
+        List<char> op = new List<char>();
+        foreach (var ch in str)
+        {
+            if (!op.Contains(ch))
+            {
+                op.Add(ch);
+            }
+        }
+        return new string(op.ToArray());
+    }
+}
